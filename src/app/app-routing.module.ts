6bf42405906f624d101cbe67facea5a8bd4041fb
Guilherme <gui.bm.1995@gmail.com>
2022-04-07 15:57:31 -0300

    only can create pdf resume in dev route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ExampleGuard } from './_shared/guards/example.guard';
+import { environment } from './../environments/environment';
+import { PdfComponent } from './pdf/pdf.component';
 
 const routes: Routes = [
   { path: '', component: PortfolioComponent },
   { path: '**', component: ErrorComponent }
 ];
 
+const routesDev: Routes = [
+  { path: '', component: PortfolioComponent },
+  { path: 'pdf', component: PdfComponent },
+  { path: '**', component: ErrorComponent }
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(environment.production ? routes : routesDev)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
